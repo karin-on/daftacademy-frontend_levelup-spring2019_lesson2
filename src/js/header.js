1c6b2fa0bfbf353aaee57d6324a4cc2326d1293f
Karina Nowicka <knowicka@gmail.com>
2019-03-31 11:37:14 +0200

    mobile first v0.1

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,18 @@
 export default () => {
     const container = document.querySelector('.container');
     const header = document.createElement('header');
+    // const innerContainer = document.createElement('div');
     const headerLogo = document.createElement('h1');
 
     header.classList.add('header');
     headerLogo.classList.add('header__logo');
     headerLogo.innerText = "d'inks";
+    // innerContainer.classList.add('inner-container');
+    //
+    // innerContainer.appendChild(headerLogo);
+    // header.appendChild(innerContainer);
 
     header.appendChild(headerLogo);
+
     container.appendChild(header);
 }
